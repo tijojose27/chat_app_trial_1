00f90f0299bf250edf2aab245cdc7021f063d483
tijo_home <tjose@wcnx.org>
2019-12-28 18:02:37 -0600

    Initial commit

---
+++ lib/LoginScreen/login_page.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginScreen extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Demo',
+      home: LoginMainScreen(),
+    );
+  }
+}
+
+class LoginMainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomPadding: true,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Stack(
+              children: <Widget>[SignOnWithLogo()],
+            ),
+          ),
+        ));
+  }
+}
+
+class SignOnWithLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          height: 200,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/chat_logo.png"))),
+        ),
+        Text(
+          "Chat App",
+          style: TextStyle(
+              fontSize: 30,
+              color: Color.fromRGBO(211, 56, 107, 1),
+              fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Container(
+          width: 300,
+          child: TextField(
+            style: TextStyle(
+              color: Color.fromRGBO(211, 56, 107, 1),
+            ),
+            decoration: InputDecoration(
+                hintText: "EMAIL",
+                prefixIcon: Icon(
+                  Icons.person_outline,
+                  color: Color.fromRGBO(211, 56, 107, 1),
+                ),
+                hintStyle: TextStyle(
+                  fontSize: 15,
+                  color: Color.fromRGBO(211, 56, 107, 1),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                  color: Color.fromRGBO(211, 56, 107, 1),
+                )),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.lightBlueAccent))),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          width: 300,
+          child: TextField(
+            style: TextStyle(
+              color: Color.fromRGBO(211, 56, 107, 1),
+            ),
+            obscureText: true,
+            decoration: InputDecoration(
+                hintText: "PASSWORD",
+                prefixIcon: Icon(
+                  Icons.lock_outline,
+                  color: Color.fromRGBO(211, 56, 107, 1),
+                ),
+                hintStyle: TextStyle(
+                  fontSize: 15,
+                  color: Color.fromRGBO(211, 56, 107, 1),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Color.fromRGBO(117, 135, 177, 1))),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.lightBlueAccent))),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          width: 300,
+          child: MaterialButton(
+            onPressed: () {},
+            textColor: Colors.white,
+            elevation: 0,
+            highlightElevation: 6,
+            splashColor: Colors.pinkAccent,
+            color: Color.fromRGBO(211, 56, 107, 1),
+            child: Text(
+              "Login",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 300,
+          child: MaterialButton(
+            onPressed: () {},
+            textColor: Colors.white,
+            elevation: 0,
+            highlightElevation: 6,
+            splashColor: Colors.pinkAccent,
+            color: Color.fromRGBO(211, 56, 107, 1),
+            child: Text(
+              "CREATE ACCOUNT",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:chat_app_trial_1/LoginScreen/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(LoginScreen());
